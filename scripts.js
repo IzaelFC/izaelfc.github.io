578d51d481cb2f2b42bb42399e0814034e580d24
IzaelFC <84091540+IzaelFC@users.noreply.github.com>
2022-12-07 16:34:13 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -18,7 +18,7 @@ function segundaPagina(){
 
 function primeiraPagina(){
     document.getElementsByTagName('main')[0].innerHTML = 
-    `<p class="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+    `<p class="texto">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
     <div class="imagem_e_botao">
         <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAJFBMVEX/+Nzw+P/////w////+vr///D19dzx+f719d7/+/r/+N7///JbBAkDAAABMUlEQVR4nO3Ptw0DARAEsZd3/ferChTcYYFVwKlgeJxz3XJdch2EhISEhISEhISEhISEhISEhISEhISEhISEhISEhISEhISEhISEhISEhISEhISEhISEhISEhISEhISEhISEhISEfyL8JIWnXNdcwSvCSYSEhLsIJxESEu4inERISLiLcBIhIeEuwkmEhIS7CCcREhLuIpxESEi4i3ASISHhLsJJhISEuwgnERIS7iKcREhIuItwEiEh4S7CSYSEhLsIJxESEu4inERYEj5yvWLAV/DqyPV+3lM938GvYEFhm/IjQkLCfoSEhP0ICQn7ERIS9iMkJOxHSEjYj5CQsB8hIWE/QkLCfoSEhP0ICQn7ERIS9iMkJOxHSEjYj5CQsB8hIWE/QkLCfoSEhP3+U/gFahDoSvNzk9YAAAAASUVORK5CYII=" alt="">
         <button type="button" onclick="segundaPagina()">CONTINUAR</button>
